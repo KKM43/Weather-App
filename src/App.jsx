82d56599d557e50976f643b250ac0acfa54da6e6
unknown <kaustubhkmore@gmail.com>
2024-01-02 09:58:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Weats from './components/Weats'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+    
+    <Weats/>
+    </>
+  )
+}
+
+export default App
